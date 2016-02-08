2df182c9ed666147226394e47144eb095a9718d4
drgizmo666 <student@cit132mac18.icl.lanecc.edu>
2016-02-08 13:44:23 -0800

    Picker Demo
    added the picker demo

---
+++ PickerAppDemo/PickerAppDemo/ViewController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using UIKit;
+
+namespace PickerAppDemo
+{
+	public partial class ViewController : UIViewController, IUIPickerViewDelegate, IUIPickerViewDataSource
+	{
+		public string[] CityNames = {"Bludhaven", "Central City", "Gorilla City", "Keystone City", "SmallVille", "Metropolis", "Star City", "Cosmos", "Gotham City", "RavenHolm"};
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public nint GetComponentCount (UIPickerView pickerView)
+		{
+			return 1;
+		}
+
+		public nint GetRowsInComponent (UIPickerView pickerView, nint component)
+		{
+			return CityNames.Length;
+
+		}
+	}
+}
+
